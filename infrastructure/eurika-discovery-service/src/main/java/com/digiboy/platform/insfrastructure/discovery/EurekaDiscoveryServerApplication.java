3a0170eeac10127007cdcd96ade07349e2e7afc7
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-10-02 14:30:36 +0200

    Eureka discovery service

---
+++ infrastructure/eurika-discovery-service/src/main/java/com/digiboy/platform/insfrastructure/discovery/EurekaDiscoveryServerApplication.java
@@ -0,0 +1,18 @@
+package com.digiboy.platform.insfrastructure.discovery;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDiscoveryServerApplication {
+
+  private static final Logger logger = LoggerFactory.getLogger(EurekaDiscoveryServerApplication.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
+  }
+}
