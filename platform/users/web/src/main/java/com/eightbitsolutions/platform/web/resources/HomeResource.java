7d01612175deac8cfd87108ab60289bfef3606cd
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-12-14 15:44:02 +0100

    Spring Cloud Api Gateway
    Add HomeController

---
+++ platform/users/web/src/main/java/com/eightbitsolutions/platform/web/resources/HomeResource.java
@@ -0,0 +1,18 @@
+package com.eightbitsolutions.platform.web.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+
+    @Autowired
+    private Environment env;
+
+    @GetMapping("/index")
+    public String index() {
+        return "Working on port " + env.getProperty("local.server.port");
+    }
+}
