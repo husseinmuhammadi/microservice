2d1c2e320bb7d34efe4c75afbeb2ea4e6d358813
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-12-15 22:11:11 +0100

    Spring Cloud Api Gateway
    Update configuration for custom routes

--- platform/users/web/src/main/java/com/eightbitsolutions/platform/web/resources/HomeResource.java
+++ platform/users/web/src/main/java/com/eightbitsolutions/platform/web/resources/HomeResource.java
@@ -3,9 +3,11 @@ package com.eightbitsolutions.platform.web.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class HomeResource {
 
     @Autowired
