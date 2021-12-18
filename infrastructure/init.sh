b120b26dcb01755bd07ccd337eea0776432709c6
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-12-18 20:53:53 +0100

    Add docker-compose for postgresql

---
+++ infrastructure/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER digiboy WITH PASSWORD '123456';
+    CREATE DATABASE digiboy;
+    GRANT ALL PRIVILEGES ON DATABASE digiboy TO digiboy;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "digiboy" --dbname "digiboy" <<-EOSQL
+    CREATE SCHEMA master;
+	CREATE SCHEMA users;
+EOSQL
