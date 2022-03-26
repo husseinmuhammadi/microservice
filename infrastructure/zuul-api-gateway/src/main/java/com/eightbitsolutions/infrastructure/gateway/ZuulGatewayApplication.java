049335324f8f8f1a08086bd495790baec23a8c17
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-03-26 22:27:03 +0100

    Spring Cloud Config Server

--- infrastructure/zuul-api-gateway/src/main/java/com/eightbitsolutions/infrastructure/gateway/ZuulGatewayApplication.java
+++ infrastructure/zuul-api-gateway/src/main/java/com/eightbitsolutions/infrastructure/gateway/ZuulGatewayApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-public class GatewayApplication {
+public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(ZuulGatewayApplication.class, args);
     }
 
 }
