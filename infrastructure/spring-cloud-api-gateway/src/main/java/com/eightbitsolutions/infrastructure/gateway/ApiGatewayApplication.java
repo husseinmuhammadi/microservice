1f1f6c1fcf97e2b1bb6feba8abd1dc944ef52f0f
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-04-14 19:46:58 +0200

    Rename to ApiGatewayApplication

--- infrastructure/spring-cloud-api-gateway/src/main/java/com/eightbitsolutions/infrastructure/gateway/ApiGatewayApplication.java
+++ infrastructure/spring-cloud-api-gateway/src/main/java/com/eightbitsolutions/infrastructure/gateway/ApiGatewayApplication.java
@@ -6,10 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class GatewayApplication {
+
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
 }
