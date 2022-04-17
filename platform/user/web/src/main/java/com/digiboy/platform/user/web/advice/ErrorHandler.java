9bb5ae15622130d86409827c5a4ee4bb7cbef39e
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-04-17 10:26:01 +0200

    Error Handling

--- platform/user/web/src/main/java/com/digiboy/platform/user/web/advice/ErrorHandler.java
+++ platform/user/web/src/main/java/com/digiboy/platform/user/web/advice/ErrorHandler.java
@@ -4,18 +4,15 @@ import com.digiboy.platform.user.exception.UsernameAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
-@RestControllerAdvice
-//@ResponseBody
-public class Error extends ResponseEntityExceptionHandler {
+@ResponseBody
+@ControllerAdvice
+public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ AccessDeniedException.class })
     public ResponseEntity<Object> handleAccessDeniedException(Exception ex, WebRequest request) {
@@ -23,8 +20,8 @@ public class Error extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({UsernameAlreadyExistsException.class})
-//    @ResponseStatus(HttpStatus.NOT_MODIFIED)
-    public ResponseEntity<ErrorMessage> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException e, WebRequest request){
-        return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorMessage(100, new Date(), e.getMessage(), ""));
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorMessage handleUsernameAlreadyExistsException(UsernameAlreadyExistsException e, WebRequest request){
+        return  new ErrorMessage(100, new Date(), e.getMessage(), "");
     }
 }
