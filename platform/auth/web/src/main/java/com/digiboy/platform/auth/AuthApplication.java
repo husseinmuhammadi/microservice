e60e942270db94c949b64ef556c71c06025bee2b
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-04-19 08:20:02 +0200

    Auth Service

--- platform/auth/web/src/main/java/com/digiboy/platform/auth/AuthApplication.java
+++ platform/auth/web/src/main/java/com/digiboy/platform/auth/AuthApplication.java
@@ -2,10 +2,12 @@ package com.digiboy.platform.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class Application {
+public class AuthApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(AuthApplication.class, args);
     }
 }
