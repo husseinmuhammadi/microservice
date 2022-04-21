1ef50745ad9cd3a5ae9752b19412228a583a077d
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-04-21 20:08:19 +0200

    Auth service call users using feign

--- platform/auth/web/src/main/java/com/digiboy/platform/auth/web/resources/LoginResource.java
+++ platform/auth/web/src/main/java/com/digiboy/platform/auth/web/resources/LoginResource.java
@@ -1,12 +1,23 @@
 package com.digiboy.platform.auth.web.resources;
 
+import com.digiboy.platform.auth.generated.v1.model.CreateUserRequest;
+import com.digiboy.platform.auth.generated.v1.model.CreateUserResponse;
+import com.digiboy.platform.auth.web.client.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginResource {
+
+    @Autowired
+    UsersService usersService;
+
     @GetMapping("/check")
     public String check(){
-        return "Hello";
+        ResponseEntity<CreateUserResponse> response = usersService.saveUser(new CreateUserRequest().email("g@g").password("1").confirmPassword("1"));
+        System.out.println("--->"+response.getStatusCode());
+        return usersService.index();
     }
 }
