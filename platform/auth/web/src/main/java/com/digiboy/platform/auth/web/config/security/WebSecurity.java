a3eb3cf6795eb37ffdad67e2dac441389f5b739e
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-04-26 04:45:56 +0200

    Web Security Config

--- platform/auth/web/src/main/java/com/digiboy/platform/auth/web/config/security/WebSecurity.java
+++ platform/auth/web/src/main/java/com/digiboy/platform/auth/web/config/security/WebSecurity.java
@@ -2,8 +2,6 @@ package com.digiboy.platform.auth.web.config.security;
 
 import com.digiboy.platform.auth.api.UserService;
 import com.digiboy.platform.auth.web.config.security.filters.AuthenticationFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,34 +13,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
 
-    public WebSecurity(UserService userService, PasswordEncoder passwordEncoder) {
+    public WebSecurity(UserService userService) {
         this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
     }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//
-////        http.addFilterAt(new AuthenticationFilter(super.authenticationManager()), UsernamePasswordAuthenticationFilter.class);
-//        // http.addFilter(new AuthenticationFilter(super.authenticationManager()));
-//
-//        http.authorizeRequests()
-//                .anyRequest().permitAll();
-//    }
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        super.configure(http);
         http.csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .addFilter(new AuthenticationFilter(authenticationManager()))
