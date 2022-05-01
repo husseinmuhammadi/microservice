915e8cc9ee5c0dc1a140fd35c5ca546468ce5ffd
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-05-01 10:52:09 +0200

    User Api Client (Feign Client)

--- platform/auth/web/src/main/java/com/digiboy/platform/auth/web/client/UsersService.java
+++
@@ -1,9 +0,0 @@
-package com.digiboy.platform.auth.web.client;
-
-import com.digiboy.platform.auth.generated.v1.api.DefaultApi;
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "users", path = "/api/v1")
-public interface UsersService extends DefaultApi {
-
-}
