166ab1be6e2eedd9a730d0d9a4b09e65631eb5a8
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-05-10 17:18:56 +0200

    Error handler

--- platform/user/web/src/main/java/com/digiboy/platform/user/web/advice/ErrorHandler.java
+++ platform/user/web/src/main/java/com/digiboy/platform/user/web/advice/ErrorHandler.java
@@ -4,24 +4,49 @@ import com.digiboy.platform.user.exception.UsernameAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Date;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @ResponseBody
 @ControllerAdvice
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({ AccessDeniedException.class })
+    @ExceptionHandler({AccessDeniedException.class})
     public ResponseEntity<Object> handleAccessDeniedException(Exception ex, WebRequest request) {
         return new ResponseEntity<>("Access denied message here", HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler({UsernameAlreadyExistsException.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorMessage handleUsernameAlreadyExistsException(UsernameAlreadyExistsException e, WebRequest request){
-        return  new ErrorMessage(100, new Date(), e.getMessage(), "");
+    public ErrorMessage handleUsernameAlreadyExistsException(UsernameAlreadyExistsException e, WebRequest request) {
+        return new ErrorMessage(e.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage handleConstraintViolationException(ConstraintViolationException e) {
+
+        System.out.println(e.getMessage());
+
+        Set<ConstraintViolation<?>> constraintViolations = e.getConstraintViolations();
+
+        Set<String> messages = new HashSet<>(constraintViolations.size());
+        messages.addAll(constraintViolations.stream()
+                .map(constraintViolation -> String.format("%s: '%s' %s",
+                        constraintViolation.getPropertyPath(),
+                        constraintViolation.getInvalidValue(),
+                        constraintViolation.getMessage())
+                ).collect(Collectors.toList()));
+        return new ErrorMessage(messages);
     }
 }
