28a5e66a066d67dcfed77714f81e93752d6dada6
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-05-10 17:08:47 +0200

    User services

--- platform/user/api/src/main/java/com/digiboy/platform/user/api/UserService.java
+++ platform/user/api/src/main/java/com/digiboy/platform/user/api/UserService.java
@@ -4,12 +4,16 @@ import com.digiboy.platform.user.dto.UserDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface UserService {
     Page<UserDTO> findAll(Pageable pageable);
 
     UserDTO findByEmail(String email);
 
     UserDTO findByUsername(String username);
+
+    List<UserDTO> findByEmailAndUsername(String email, String username);
 
     UserDTO save(UserDTO userDTO);
 
