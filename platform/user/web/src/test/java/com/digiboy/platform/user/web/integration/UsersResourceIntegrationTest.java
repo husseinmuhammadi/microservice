0c34e64192af1e8618e4694f1a137c0854c702f0
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-05-12 16:59:20 +0200

    New test for find user by email

--- platform/user/web/src/test/java/com/digiboy/platform/user/web/integration/UsersResourceIntegrationTest.java
+++ platform/user/web/src/test/java/com/digiboy/platform/user/web/integration/UsersResourceIntegrationTest.java
@@ -1,15 +1,35 @@
 package com.digiboy.platform.user.web.integration;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+import javax.ws.rs.core.MediaType;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Transactional
+@AutoConfigureCache
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase
+@AutoConfigureTestEntityManager
+@ImportAutoConfiguration
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class UsersResourceIntegrationTest {
 
@@ -35,4 +55,19 @@ class UsersResourceIntegrationTest {
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 
+    @Test
+    @Sql("classpath:sql-data/insert-single-user.sql")
+    void shouldFindUser() throws Exception {
+
+        final String email = "info@example.com";
+
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/users/{email}/email", email))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("04017bf8-cadc-11ec-a70f-acde48001122")))
+                .andReturn().getResponse();
+
+        assertEquals(MediaType.APPLICATION_JSON,  response.getContentType());
+    }
 }
---
+++ platform/user/web/src/test/resources/sql-data/insert-single-user.sql
@@ -0,0 +1,1 @@
+insert into users (user_id, username, password, email) select '04017bf8-cadc-11ec-a70f-acde48001122', 'u01', 'P@ssw0rd', 'info@example.com';
