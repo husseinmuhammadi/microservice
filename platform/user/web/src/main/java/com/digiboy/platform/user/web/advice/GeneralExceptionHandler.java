7cbac2e343ab525baa533cff43661a66856b9912
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-05-13 16:03:21 +0200

    Add the constraint violation message to the body of the http response

--- platform/user/web/src/main/java/com/digiboy/platform/user/web/advice/GeneralExceptionHandler.java
+++ platform/user/web/src/main/java/com/digiboy/platform/user/web/advice/GeneralExceptionHandler.java
@@ -3,9 +3,11 @@ package com.digiboy.platform.user.web.advice;
 import com.digiboy.platform.user.exception.ServiceException;
 import com.digiboy.platform.user.exception.UserNotFoundException;
 import com.digiboy.platform.user.exception.UsernameAlreadyExistsException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,21 +21,22 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@ResponseBody
 @ControllerAdvice
 public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({AccessDeniedException.class})
-    public ResponseEntity<Object> handleAccessDeniedException(Exception e, WebRequest request) {
+    public ResponseEntity<Object> handleAccessDeniedException(AccessDeniedException e, WebRequest request) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied message here");
     }
 
+    @ResponseBody
     @ExceptionHandler({UsernameAlreadyExistsException.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorMessage handleUsernameAlreadyExistsException(UsernameAlreadyExistsException e, WebRequest request) {
         return new ErrorMessage(e.getMessage());
     }
 
+    @ResponseBody
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorMessage handleConstraintViolationException(ConstraintViolationException e) {
@@ -49,9 +52,15 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
         return new ErrorMessage(messages);
     }
 
+    @ResponseBody
     @ExceptionHandler({UserNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ServiceErrorMessage handleUserNotFound(ServiceException e) {
         return new ServiceErrorMessage(e.getErrorCode(), e.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleExceptionInternal(ex, ex.getMessage(), headers, status, request);
     }
 }
