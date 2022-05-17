b0b66ab604dc0abd4dade19423d0bc8c1805b500
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-05-17 14:37:54 +0200

    Spring Library

---
+++ platform/spring-library/web/src/test/java/com/digitalstudio/platform/SpringLibraryApplicationTest.java
@@ -0,0 +1,14 @@
+package com.digitalstudio.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpringLibraryApplicationTest {
+
+    @Test
+    void loadContext(){
+    }
+}
