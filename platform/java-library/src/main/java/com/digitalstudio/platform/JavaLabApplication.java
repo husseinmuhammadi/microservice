970a93dcf0f08410ac9a1e42bdbb861be44752a9
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-05-17 14:37:24 +0200

    Java Library

---
+++ platform/java-library/src/main/java/com/digitalstudio/platform/JavaLabApplication.java
@@ -0,0 +1,7 @@
+package com.digitalstudio.platform;
+
+public class JavaLabApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ platform/java-library/src/test/java/com/digitalstudio/platform/JavaLabApplicationTest.java
@@ -0,0 +1,7 @@
+package com.digitalstudio.platform;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaLabApplicationTest {
+
+}
