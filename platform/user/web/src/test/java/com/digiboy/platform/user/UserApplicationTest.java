cf9f1b44c98268f5a5571df6fd73d7c97754f1b4
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-06-26 17:04:26 +0200

    decorate UserApplicationTest with @IntegrationTest

--- platform/user/web/src/test/java/com/digiboy/platform/user/UserApplicationTest.java
+++ platform/user/web/src/test/java/com/digiboy/platform/user/UserApplicationTest.java
@@ -1,10 +1,10 @@
 package com.digiboy.platform.user;
 
+import com.digiboy.platform.user.web.integration.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+@IntegrationTest
 @SpringBootTest
 class UserApplicationTest {
 
