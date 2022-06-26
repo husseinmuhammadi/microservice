4c3207027a6cc0c30c877042fab7945424d59e35
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-06-26 16:32:54 +0200

    run integration test with test profile

--- platform/user/web/src/test/java/com/digiboy/platform/user/UserApplicationTest.java
+++ platform/user/web/src/test/java/com/digiboy/platform/user/UserApplicationTest.java
@@ -1,12 +1,10 @@
 package com.digiboy.platform.user;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@ActiveProfiles("test")
 @SpringBootTest
 class UserApplicationTest {
 
