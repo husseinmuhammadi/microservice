e57925071c611a130d16434f2f64beb0eea67987
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-06-26 21:24:47 +0200

    Users Microservice Integration Test
    Create context based upon test profile

--- platform/user/web/src/test/java/com/digiboy/platform/user/UserApplicationTest.java
+++ platform/user/web/src/test/java/com/digiboy/platform/user/UserApplicationTest.java
@@ -2,10 +2,8 @@ package com.digiboy.platform.user;
 
 import com.digiboy.platform.user.web.integration.IntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @IntegrationTest
-@SpringBootTest
 class UserApplicationTest {
 
     @Test
