15a625d03467772cb4cc4fcdd6d2b8c2523cd109
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2022-06-28 08:11:40 +0200

    29 deploy users micro service on stage environment (#30)
    * Users microservice
    
    * remove users module
    
    * disable eureka discovery service and spring cloud config on test profile
    
    * clean code
    
    * remove maven profiles related to spring active profiles
    
    * run integration test with test profile
    
    * decorate UserApplicationTest with @IntegrationTest
    
    * readme
    
    * Users Microservice Integration Test
    
    Create context based upon test profile
    
    * Users Microservice Integration Test
    
    Make all test methods transactional
    
    * platform docker compose
    
    Co-authored-by: Hossein Mohammadi <hmohammadi@mobiquityinc.com>

--- platform/user/web/src/test/java/com/digiboy/platform/user/UserApplicationTest.java
+++ platform/user/web/src/test/java/com/digiboy/platform/user/UserApplicationTest.java
@@ -1,13 +1,9 @@
 package com.digiboy.platform.user;
 
+import com.digiboy.platform.user.web.integration.IntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+@IntegrationTest
 class UserApplicationTest {
 
     @Test
