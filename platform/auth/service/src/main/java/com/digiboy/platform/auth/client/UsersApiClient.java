4430af327c3303b16cb627015bad65f9cdae247f
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-07-01 22:20:14 +0200

    Spring Cloud Config Server
    Run all services on dev environment

--- platform/auth/service/src/main/java/com/digiboy/platform/auth/client/UsersApiClient.java
+++ platform/auth/service/src/main/java/com/digiboy/platform/auth/client/UsersApiClient.java
@@ -2,9 +2,9 @@ package com.digiboy.platform.auth.client;
 
 import com.digiboy.platform.auth.generated.v1.api.DefaultApi;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
 
 // todo: move this class to packaage com.digiboy.platform.user.client
-@FeignClient(name = "users", path = "/api/v1")
+@FeignClient(name = "${client.services.users-api.name}", path = "/api/v1")
 public interface UsersApiClient extends DefaultApi {
-
 }
